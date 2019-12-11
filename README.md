87aa80697e1237e6c1197871012cdeb061015fb7
Alex Mok <mokalexander00@gmail.com>
2019-12-11 16:57:17 -0500

    feat: expires data

---
+++ README.md
@@ -0,0 +1,12 @@
+#Stellar
+API accepts JSON format data
+
+Routes are:
+Get - api/snippets/id
+Uses a number for id, will fetch specific snippet
+
+Post - api/snippets
+Creates a new snippet and returns url for new snippet
+
+Put - api/snippets/like
+Uses a number for id, will increment like on that post
--- server/snippets.js
+++ server/snippets.js
@@ -1,20 +1,36 @@
 const router = require('express').Router();
 const snippets = { '1': { data: 'Test Format', likes: 0 } };
+const EXPIRE_TIME = 10000;
 
-router.get('/', (req, res) => {
-  res.json(snippets);
-});
+function expireData(id) {
+  snippets[id] = { data: 'Data has expired' };
+}
+
+// router.get('/', (req, res) => {
+//   console.log(snippets);
+//   res.json(snippets);
+// });
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
+  let expireFunction = snippets[id].expire;
+  if (expireFunction) {
+    clearTimeout(expireFunction);
 
-  res.json(snippets[id]);
+    expireFunction = setTimeout(() => expireData(id), EXPIRE_TIME);
+    snippets[id].expire = expireFunction;
+  }
+
+  res.json({ data: snippets[id].data, like: snippets[id].likes });
 });
 
 router.post('/', (req, res) => {
   let length = Object.keys(snippets).length + 1;
   const newSnippet = req.body;
   newSnippet.likes = 0;
+
+  const expireFunction = setTimeout(() => expireData(length), EXPIRE_TIME);
+  newSnippet.expire = expireFunction;
 
   snippets[length] = newSnippet;
 
@@ -25,7 +41,6 @@ router.post('/', (req, res) => {
 router.put('/like/:id', (req, res) => {
   const id = req.params.id;
 
-  //Would normally be a server put request
   snippets[id].likes++;
 
   res.json(snippets[id]);
